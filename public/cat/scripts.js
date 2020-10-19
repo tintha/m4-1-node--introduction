28c3d59896f10911abe40cfd5532d69156165332
tintha <tintha@gmail.com>
2020-10-19 15:51:13 -0400

    Ex 1

--- public/cat/scripts.js
+++ public/cat/scripts.js
@@ -1,1 +1,40 @@
 // here there be JS, yarrr ☠️
+const messageInput = document.querySelector('#user-input');
+const conversationElem = document.querySelector('#conversation-container');
+
+const handleFocus = () => {
+  messageInput.focus();
+}
+
+const updateConversation = (message) => {
+  // deconstruct the message object
+  const { author, text } = message;
+  // create a <p> element
+  const messageElem = document.createElement("p");
+  messageElem.classList.add("message", author);
+  // add the text message to the element
+  messageElem.innerHTML = `<span>${text}</span>`;
+  // append the element to the conversation
+  conversationElem.appendChild(messageElem);
+  conversationElem.scrollTop = conversationElem.scrollHeight;
+  if (author === "user") {
+    messageInput.value = "";
+  }
+  handleFocus();
+}
+
+const sendMessage = (event) => {
+  // prevent the default "page reload" from occurring.
+  event.preventDefault();
+  const message = { author: 'user', text: messageInput.value };
+  updateConversation(message);
+
+  // This is a 'GET' call to the /cat-message endpoint.
+  fetch("/cat-message")
+  .then((res) => res.json())
+  .then((data) => {
+  updateConversation(data.message);
+  });
+};
+
+handleFocus();
--- server.js
+++ server.js
@@ -17,7 +17,13 @@ express()
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
-
+  .get('/cat-message', (req, res) => {
+    const message = { author: 'cat', text: 'Meow' };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({status: 200, message });
+    }, randomTime);
+  })
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
