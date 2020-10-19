cf82431f95173aa3424ab575b4cf09ef42b152e1
tintha <tintha@gmail.com>
2020-10-19 17:33:15 -0400

    Ex 2

--- server.js
+++ server.js
@@ -24,6 +24,25 @@ express()
       res.status(200).json({status: 200, message });
     }, randomTime);
   })
+
+  .get('/monkey-message', (req, res) => {
+    const messages = [
+      "Don’t monkey around with me.",
+      "If you pay peanuts, you get monkeys.",
+      "I fling 💩 at you!",
+      "🙊",
+      "🙈",
+      "🙉",
+      "I go bananas for bananas!"
+    ];
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    const message = { author: 'monkey', text: randomMessage };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({status: 200, message });
+    }, randomTime);
+  }
+  )
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
